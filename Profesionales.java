3c2385699c6cb7f5a560cf23d838a061c0b52b97
ajopmo <102189240+ajopmo@users.noreply.github.com>
2022-04-18 22:32:06 -0600

    Clase Asinc
    Parte Andres Picado

---
+++ Profesionales.java
@@ -0,0 +1,66 @@
+//Andres picado monge
+
+package Personas;
+
+import java.util.HashSet;
+import javax.swing.JOptionPane;
+
+public class Profesionales extends Profesores {
+    private String titulos;
+    private String grado;
+    private String materia;   
+    
+    
+    public Profesionales(){
+        
+    }
+
+    public Profesionales(String titulos, String grado, String materia, String nombre, int edad, char genero, String profesion) {
+        super(nombre, edad, genero, profesion);
+        this.titulos = titulos;
+        this.grado = grado;
+        this.materia = materia;   
+    }
+
+    
+
+    public String getTitulos() {
+        return titulos;
+    }
+
+    public void setTitulos(String titulos) {
+        this.titulos = titulos;
+    }
+
+    public String getGrado() {
+        return grado;
+    }
+
+    public void setGrado(String grado) {
+        this.grado = grado;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+    
+    public void imprimirDatos(){
+     JOptionPane.showMessageDialog(null,"El nombre del alumno es : "+getNombre()
+     +"\n la edad es : "+getEdad()
+     +"\n el genero es : "+getGenero()
+     +"\n Cuantos titulos tiene el docente : "+titulos
+     +"\n Que grado da este docente : "+grado
+     +"\n Cual materia da : "+materia);
+    
+    }
+    
+    public static void main(String[] args){
+    //parte de Nicole
+    
+    }
+}
+
